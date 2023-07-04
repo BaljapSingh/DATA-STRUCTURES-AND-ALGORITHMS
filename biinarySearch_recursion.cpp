1d4c3421a27c0ca561ae3b032065ed972ca5fdef
BaljapSingh <71628516+BaljapSingh@users.noreply.github.com>
2023-07-04 18:53:53 +0530

    Add files via upload

---
+++ biinarySearch_recursion.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+bool solve(int *arr, int size, int k)
+{
+    //base case
+    if(size == 0)
+    {
+        return false;
+    }
+    if(arr[0] == k)
+    {
+        return true;
+    }
+
+    return solve(arr+1,size-1,k);
+}
+
+int main()
+{
+    int arr[5]={1,4,7,8,5};
+    int k = 4;
+    bool ans = solve(arr,5,k);
+    cout<<endl<<endl;
+    if(ans)
+    {
+        cout<<"present"<<endl;
+    }
+    else    
+        cout<<"not present"<<endl;
+}

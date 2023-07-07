79106fc30d7665fbe6cc0e653f6714388d4c8a66
BaljapSingh <71628516+BaljapSingh@users.noreply.github.com>
2023-07-07 10:17:05 +0530

    Add files via upload

---
+++ Subsets_recursion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    void solve(vector<int> nums, int index, vector<int> output, vector<vector<int>> &ans)
+    {
+        if(index >= nums.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+
+        // exclude
+        solve(nums,index+1, output, ans);
+
+        //include
+        int digit = nums[index];
+        output.push_back(digit);
+        solve(nums, index+1 , output, ans);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+
+        solve(nums,index, output, ans);
+        return ans;
+    }
+};
